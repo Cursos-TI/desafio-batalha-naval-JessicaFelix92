864bfcd5a60309162a7cb175e2f0e1a0938dfd90
JessicaFelix92 <jessicafspl@hotmail.com>
2025-10-01 14:15:55 +0000

    Criação tabuleiro e posição navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -36,5 +36,54 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
+    char coluna [10] = {'A','B','C','D','E','F','G','H','I','J',}; //Colunas do tabuleiro
+    int tabuleiro [10][10]; //Declaração do tabuleiro 10x10
+    int tamanhoNavio = 3, navio = 3;
+
+    // Inicializar tabuleiro com 0 - água
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    //Coordenadas iniciais dos navios
+    int linhaH = 2, colunaH = 3; //Navio horizontal começa na linha 2, coluna 3
+    int linhaV = 5, colunaV = 7; //Navio vertical começa na linha 5, coluna 7
+
+    //Posição navio horizontal
+    if (colunaH + tamanhoNavio <= 10) { //Verifica se cabe no tabuleiro
+        for (int i = 0; i < tamanhoNavio; i++) {
+            tabuleiro[linhaH][colunaH + i] = navio;
+        }
+    }
+
+    //Posição navio vertical
+    if (linhaV + tamanhoNavio <= 10) { //Verifica se cabe no tabuleiro
+        for (int i = 0; i < tamanhoNavio; i++) {
+            // só posiciona se não houver outro navio na célula
+            if (tabuleiro[linhaV + i][colunaV] == 0) {
+                tabuleiro[linhaV + i][colunaV] = navio;
+            }
+        }
+    }
+
+        printf("Tabuleiro Batalha Naval\n");
+        printf("  ");
+        for (int j = 0; j < 10; j++) {
+            printf(" %c ", coluna[j]); //Imprime as colunas de A a J
+
+    }
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%2d  ", i+1); // Imprime os números das linhas 1 a 10
+        for (int j = 0; j < 10; j++) {
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
+
 }
